e243254b4fb66fa213d91d53e26e091665b40aa9
XeroHack <73324025+xerohackcom@users.noreply.github.com>
2022-02-18 18:04:21 +0800

    Update
    - Add python3 to extra.sh
    - Add information in readme

--- extra.sh
+++ extra.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt update && sudo apt install -y freerdp2-x11 python3-pip python3-libvirt && \
+sudo apt update && sudo apt install -y freerdp2-x11 python3 python3-pip python3-libvirt && \
 pip3 install PyQt5 && \
 echo "PATH=\$PATH:$HOME/.local/bin" >> $HOME/.profile
 
